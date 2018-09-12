60bc9dd0e8b62a72f80ae1649c0688cc5f28f6e5
Jordi735 <jordidorren@gmail.com>
2018-09-12 12:06:44 +0200

    added postgresql tests

---
+++ postgresql/Main.java
@@ -0,0 +1,55 @@
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        final String url = "jdbc:postgresql://80.100.250.208:42000/ziekenhuis";
+        final String user = "postgres";
+        final String pass = "dreamteam_en_bas";
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(url, user, pass);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (conn != null) {
+            System.out.println("connected");
+        }
+
+        try {
+
+            Statement stmt = conn.createStatement();
+            String sql;
+
+            sql = "INSERT INTO test VALUES ('bas')";
+
+            for (int i = 0; i < 1000; i++) {
+                stmt.execute(sql);
+            }
+
+            sql = "TRUNCATE TABLE test";
+            stmt.execute(sql);
+
+            /*
+            while (rs.next()) {
+               String nr = rs.getString("patient_nr"); 
+               System.out.println(nr);
+            }
+            */
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
---
+++ postgresql/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+javac -cp ./postgresql.jar:./ Main.java
