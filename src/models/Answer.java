1d570e39ae479df70ca10aee8b2a42d92d09798f
Jordi735 <jordidorren@gmail.com>
2018-10-10 12:20:56 +0200

    quick push, testing branch

--- src/models/Answer.java
+++ src/models/Answer.java
@@ -1,5 +1,39 @@
 package models;
 
 public class Answer {
-}
 
+    private int id;
+    private String text;
+    private String url;
+
+    public Answer(int id, String text, String url) {
+        this.id = id;
+        this.text = text;
+        this.url = url;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+}
