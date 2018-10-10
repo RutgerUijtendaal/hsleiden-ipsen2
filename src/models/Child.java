14fc9a49bf5eff469cfbf75da0d38f358731f488
Jordi735 <jordidorren@gmail.com>
2018-10-10 14:11:21 +0200

    free lines for me - added all the models

--- src/models/Child.java
+++ src/models/Child.java
@@ -1,5 +1,40 @@
 package models;
 
-public class Child {
-}
+import java.util.Date;
 
+public class Child {
+
+    private int coupleId;
+    private Date date;
+    private boolean isBorn;
+
+    public Child (int coupleId, Date date, boolean isBorn) {
+        this.coupleId = coupleId;
+        this.date = date;
+        this.isBorn = isBorn;
+    }
+
+    public void setCoupleId(int coupleId) {
+        this.coupleId = coupleId;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setIsBorn(boolean isBorn) {
+        this.isBorn = isBorn;
+    }
+
+    public int getCoupleId() {
+        return coupleId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public boolean isBorn() {
+        return isBorn;
+    }
+}
--- src/models/Parent.java
+++ src/models/Parent.java
@@ -1,5 +1,28 @@
 package models;
 
 public class Parent {
-}
 
+    private String phoneNr;
+    private String firstName;
+
+    public Parent(String phoneNr, String firstName) {
+        this.phoneNr = phoneNr;
+        this.firstName = firstName;
+    }
+
+    public String getPhoneNr() {
+        return phoneNr;
+    }
+    
+    public void setPhoneNr(String phoneNr) {
+        this.phoneNr = phoneNr;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
