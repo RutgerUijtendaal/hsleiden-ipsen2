78cbfad7f76278e0a5ca143bb7fdc561d85cdacd
Jordi735 <jordidorren@gmail.com>
2018-10-10 22:06:04 +0200

    wrote the logic to find match the parents in all couples, TODO = the Dao logic

--- src/controllers/CoupleListController.java
+++ src/controllers/CoupleListController.java
@@ -1,7 +1,15 @@
 package controllers;
 
 import views.CoupleListView;
+import daos.ConnectionFactory;
+import daos.DaoManager;
+import daos.ParentDao;
+import daos.CoupleDao;
 import views.BaseView;
+import models.Parent;
+import models.Couple;
+
+import java.util.List;
 
 public class CoupleListController {
 
@@ -18,6 +26,38 @@ public class CoupleListController {
     }
 
     public void handleSearchBtnClick(String email) {
+        if (email.isEmpty()) {
+
+            ParentDao parentDao = DaoManager.getParentDao();
+            CoupleDao coupleDao = DaoManager.getCoupleDao();
+            List<Parent> allParents = parentDao.getAll();
+            List<Couple> allCouples = coupleDao.getAll();
+            parentDao = null;
+            coupleDao = null;
+
+            String tmpEmail1 = null;
+            String tmpEmail2 = null;
+
+            for (Couple currCouple : allCouples) {
+                int id1 = currCouple.getParentId1();
+                int id2 = currCouple.getParentId2();
+                for (Parent currParent : allParents) {
+                    int parentId = currParent.getId();
+                    if (parentId == id1 || parentId == id2) {
+                        if (tmpEmail1 == null) {
+                            tmpEmail1 = currParent.getEmail();
+                        } else {
+                            tmpEmail2 = currParent.getEmail();
+                        }
+                    }
+                }
+            }
+
+            clv.addSingleRow(tmpEmail1, tmpEmail2);
+
+        } else {
+
+        }
     }
 
     public void handleBackBtnClick() {
