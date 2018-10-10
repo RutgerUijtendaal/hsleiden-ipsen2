fa543aa9acbe454f65cecdde658289794d5bcfa4
Jordi735 <jordidorren@gmail.com>
2018-10-10 21:44:25 +0200

    added filling of the cells, plus getting the emails from clicking on the delete button

--- src/controllers/CoupleListController.java
+++ src/controllers/CoupleListController.java
@@ -23,4 +23,5 @@ public class CoupleListController {
     public void handleBackBtnClick() {
         appCtl.switchToAdminMenuView();
     }
+
 }
--- src/views/CoupleListView.java
+++ src/views/CoupleListView.java
@@ -1,14 +1,18 @@
 package views;
 
+
+
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import java.io.File;
 import javafx.fxml.FXML;
-//import javafx.geometry.Insets;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.Parent;
-import javafx.scene.image.ImageView;
-import javafx.geometry.Pos;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
@@ -40,13 +44,13 @@ public class CoupleListView extends BaseView {
 
     private ObservableList<HBox> listData;
 
+    double smallChange = 1.05;
+    double bigChange = 1.1;
+
     public CoupleListView(CoupleListController clc) {
         this.clc = clc;
         rootFXML = super.loadFXML("../fxml/parent_list.fxml");
         rootScene = new Scene(rootFXML, 1280, 720);
-
-        double smallChange = 1.05;
-        double bigChange = 1.1;
             
         super.setScaleTransitions(searchBtn, smallChange);
         super.setScaleTransitions(backBtn, smallChange);
@@ -60,25 +64,13 @@ public class CoupleListView extends BaseView {
         super.setScaleTransitions(logoO, bigChange);
 
         listData = FXCollections.observableArrayList();
-
-        Region region1 = new Region();
-        HBox.setHgrow(region1, Priority.ALWAYS);
-
-        HBox mainBox = new HBox();
-        HBox rightBox = new HBox();
-        VBox leftBox = new VBox();
-        mainBox.setStyle("-fx-background-color: red");
-        rightBox.setStyle("-fx-background-color: yellow");
-        leftBox.setStyle("-fx-background-color: green");
-        mainBox.getChildren().addAll(leftBox, region1, rightBox);
-
-
-        leftBox.getChildren().addAll(new Label("ouder1@jemoeder.net"), new Label("ouder2@coolapplicatie.com"));
-        rightBox.getChildren().add(new Label("delete dis"));
-        rightBox.setAlignment(Pos.CENTER_RIGHT);
-
-        listData.add(mainBox);
         resultsList.setItems(listData);
+
+        addSingleRow("email1", "this is a parent");
+        addSingleRow("goodmciwail", "i wwaerrr");
+        addSingleRow("email2", "emailllll");
+        addSingleRow("aslkjeflj", "this is a parent");
+
     }
 
     public Scene getViewScene() {
@@ -95,9 +87,46 @@ public class CoupleListView extends BaseView {
         clc.handleBackBtnClick();
     }
 
-    public void setListData(ObservableList<HBox> listData) {
-        this.listData = listData;
-        resultsList.setItems(listData);
+    public void clearListData() {
+        listData.clear();
+    }
+
+    private void getEmailFromClick(ImageView deleteImgView) {
+        HBox firstParent = (HBox)deleteImgView.getParent();
+        HBox secondParent = (HBox)firstParent.getParent();
+        VBox emailParent = (VBox)secondParent.getChildren().get(0);
+        Label emailLabel1 = (Label)emailParent.getChildren().get(0);
+        Label emailLabel2 = (Label)emailParent.getChildren().get(1);
+        System.out.println(emailLabel1.getText());
+        System.out.println(emailLabel2.getText());
+    }
+
+    public void addSingleRow(String email1, String email2) { // THIS COULD ALSO JUST TAKE PARENTS FOR EASIER GETTING OF THE ID
+
+        Region spacer = new Region();
+        HBox mainBox = new HBox();
+        HBox rightBox = new HBox();
+        VBox leftBox = new VBox();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+        leftBox.setSpacing(1);
+
+        mainBox.getChildren().addAll(leftBox, spacer, rightBox);
+        Image deleteImg = new Image(this.getClass().getResourceAsStream("../resources/delete.png"));
+        ImageView deleteImgView = new ImageView(deleteImg);
+        deleteImgView.setFitHeight(50);
+        deleteImgView.setFitWidth(50);
+        super.setScaleTransitions(deleteImgView, bigChange);
+
+        leftBox.getChildren().addAll(new Label(email1), new Label(email2));
+        rightBox.getChildren().add(deleteImgView);
+        rightBox.setAlignment(Pos.CENTER_RIGHT);
+
+        listData.add(mainBox);
+
+        deleteImgView.setOnMouseClicked( (MouseEvent e ) -> {
+            getEmailFromClick(deleteImgView);
+        });
+
     }
 
 }
