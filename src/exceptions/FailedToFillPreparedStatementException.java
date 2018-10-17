6ad0dde4872de19b2241e8bf26d4c90f478c4b3f
Jordi735 <jordidorren@gmail.com>
2018-10-17 19:13:37 +0200

    added serialVersionUID to silence my overly active java linter

--- src/exceptions/FailedToFillPreparedStatementException.java
+++ src/exceptions/FailedToFillPreparedStatementException.java
@@ -2,6 +2,8 @@ package exceptions;
 
 public class FailedToFillPreparedStatementException extends PreparedStatementException {
 
+    private final static long serialVersionUID = 25005L;
+
     public FailedToFillPreparedStatementException() {
         super("Failed to fill a ");
     }
--- src/exceptions/PreparedStatementException.java
+++ src/exceptions/PreparedStatementException.java
@@ -2,6 +2,8 @@ package exceptions;
 
 public class PreparedStatementException extends DatabaseException {
 
+    private final static long serialVersionUID = 25009L;
+
     public PreparedStatementException(String message) {
         super(message);
     }
