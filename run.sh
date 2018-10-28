340b825d8fd9239a3b39d330e190c920df27ff08
Unknown <stefandekeijzer@xs4all.nl>
2018-10-28 17:11:59 +0100

    Small change to build and run scripts, now loads every lib without adding them manually

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -cp lib/joda-time-2.10.jar:lib/commons-io-2.6.jar:lib/commons-logging-1.2.jar:lib/httpclient-4.5.6.jar:lib/httpcore-4.4.10.jar:lib/httpmime-4.5.6.jar:lib/javax.mail.jar:lib/postgres.jar:out/ main.Main
+java -cp "lib/*"::lib/postgres.jar:out/ main.Main
 
