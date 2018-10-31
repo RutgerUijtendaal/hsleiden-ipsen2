a628488a5e40e798c99bafb623bdf9eb3af4bfa5
BasdeBruyn <37217983+BasdeBruyn@users.noreply.github.com>
2018-10-31 11:57:30 +0100

    Added extra right to the result model and dao

--- src/models/Right.java
+++ src/models/Right.java
@@ -5,16 +5,19 @@ public class Right implements DatabaseObject<Right> {
     private int id;
     private boolean canEditDilemma;
     private boolean canViewStatistics;
+    private boolean canEditUserInfo;
 
-    public Right(boolean can_edit_dilemma, boolean can_view_statistics) {
+    public Right(boolean can_edit_dilemma, boolean can_view_statistics, boolean canEditUserInfo) {
         this.canEditDilemma = can_edit_dilemma;
         this.canViewStatistics = can_view_statistics;
+        this.canEditUserInfo = canEditUserInfo;
     }
 
-    public Right(int id, boolean can_edit_dilemma, boolean can_view_statistics){
+    public Right(int id, boolean can_edit_dilemma, boolean can_view_statistics, boolean canEditUserInfo){
         this.id = id;
         this.canEditDilemma = can_edit_dilemma;
         this.canViewStatistics = can_view_statistics;
+        this.canEditUserInfo = canEditUserInfo;
     }
 
     public int getId() {
@@ -39,6 +42,14 @@ public class Right implements DatabaseObject<Right> {
 
     public void setCanViewStatistics(boolean canViewStatistics) {
         this.canViewStatistics = canViewStatistics;
+    }
+
+    public boolean isCanEditUserInfo() {
+        return canEditUserInfo;
+    }
+
+    public void setCanEditUserInfo(boolean canEditUserInfo) {
+        this.canEditUserInfo = canEditUserInfo;
     }
 
     @Override
